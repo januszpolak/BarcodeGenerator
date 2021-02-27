557c550d1a1e7b7f54a8f12433419c5a3206c1bf
januszpolak <janusz.polak@interia.pl>
2021-02-27 14:20:05 +0100

    add print 6 barcodes on one page

--- Form1.cs
+++ Form1.cs
@@ -62,14 +62,32 @@ namespace BarcodeGenerator
 
         private void button4_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image.Save(@"D:\sample.png");
-            
+         
+            var imageTitle = label1.Text;
+            var path = "\\";
+
+           
+            pictureBox1.Image.Save(path + imageTitle + ".jpeg");
+          
+            string message = "Barcode saved !!!";
+            MessageBox.Show(message);
+         
         }
 
         private void Print_Page(object sender, PrintPageEventArgs e)
         {
             e.Graphics.DrawImage(pictureBox1.Image, 100, 100, 200, 100);
             e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(100, 250));
+            e.Graphics.DrawImage(pictureBox1.Image, 500, 100, 200, 100);
+            e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(500, 250));
+            e.Graphics.DrawImage(pictureBox1.Image, 100, 300, 200, 100);
+            e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(100, 450));
+            e.Graphics.DrawImage(pictureBox1.Image, 500, 300, 200, 100);
+            e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(500, 450));
+            e.Graphics.DrawImage(pictureBox1.Image, 100, 500, 200, 100);
+            e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(100, 650));
+            e.Graphics.DrawImage(pictureBox1.Image, 500, 500, 200, 100);
+            e.Graphics.DrawString(textBox1.Text, new Font("Arial", 16, FontStyle.Regular), Brushes.Black, new Point(500, 650));
         }
 
        
